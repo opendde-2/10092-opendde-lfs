6a6fda16acdfeebc148101d884d2831939a51a74
gnuhub <gnuhub@gmail.com>
2023-07-15 10:47:15 +0800

    auto commit

--- 1.ci.run.sh
+++ 1.ci.run.sh
@@ -2,6 +2,9 @@
 export CMD_PATH=$(cd `dirname $0`; pwd)
 cd $CMD_PATH
 
+git config user.name github-actions
+git config user.email github-actions@github.com
+
 ./2.2.version.check.sh
 
 ./3.1.wget.source.sh
