8d3df04c4c22794047024a22eb65745b20e5fcef
gnuhub <gnuhub@gmail.com>
2023-07-15 10:25:17 +0800

    auto commit

--- 3.1.wget.source.sh
+++ 3.1.wget.source.sh
@@ -2,6 +2,6 @@
 export CMD_PATH=$(cd `dirname $0`; pwd)
 cd $CMD_PATH
 . ./2.6.lfs.env.sh
-mkdir -v $LFS/sources
+mkdir -pv $LFS/sources/
 chmod -v a+wt $LFS/sources
 wget --input-file=wget-list-sysv --continue --directory-prefix=$LFS/sources
