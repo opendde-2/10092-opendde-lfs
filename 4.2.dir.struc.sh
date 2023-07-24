2c39aa1f4b2b4dc649491025019118d0b280d376
gnuhub <gnuhub@gmail.com>
2023-07-24 16:48:45 +0800

    Update 4.2.dir.struc.sh

--- 4.2.dir.struc.sh
+++ 4.2.dir.struc.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+export CMD_PATH=$(cd `dirname $0`; pwd)
+cd $CMD_PATH
 
+. ./2.6.lfs.env.sh
 mkdir -pv $LFS/{lib,var,etc,bin,sbin,tools} $LFS/usr/{bin,lib,sbin}
 
 for i in bin lib sbin; do
-  sudo ln -sv usr/$i $LFS/$i
+   ln -sv usr/$i $LFS/$i
 done
 
 case $(uname -m) in
