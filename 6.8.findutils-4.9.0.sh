73d6433cb8ecca0e5ed784bbc478e85ea8eced80
root <admin@example.com>
2023-07-25 15:45:38 +0800

    完成第六章

---
+++ 6.8.findutils-4.9.0.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+cd $LFS/sources
+tar -xf findutils-4.9.0.tar.xz
+cd findutils-4.9.0
+
+./configure --prefix=/usr                   \
+            --localstatedir=/var/lib/locate \
+            --host=$LFS_TGT                 \
+            --build=$(build-aux/config.guess)
+
+make && make DESTDIR=$LFS install
+
+cd $LFS/sources
+rm -rf findutils-4.9.0
