1af2ecd2c5bc71e7692d0133dc815837a4059829
root <admin@example.com>
2023-07-25 16:21:18 +0800

    系统环境优化

--- 3.1.wget.source.sh
+++ 3.1.wget.source.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-export CMD_PATH=$(cd `dirname $0`; pwd)
-cd $CMD_PATH
-. ./2.6.lfs.env.sh
 mkdir -pv $LFS/sources/
 chmod -v a+wt $LFS/sources
 wget --input-file=wget-list-sysv --continue --directory-prefix=$LFS/sources
--- 4.2.dir.struc.sh
+++ 4.2.dir.struc.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-export CMD_PATH=$(cd `dirname $0`; pwd)
-cd $CMD_PATH
-
-. ./2.6.lfs.env.sh
 mkdir -pv $LFS/{lib,var,etc,bin,sbin,tools} $LFS/usr/{bin,lib,sbin}
 
 for i in bin lib sbin; do
